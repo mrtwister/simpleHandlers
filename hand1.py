21eb8f01e7578bc0e9b47f2b2242ee14d3a17af2
mrtwister <guskuskudu@gmail.com>
2024-05-24 11:47:46 +0700

    Update hand1.py

--- hand1.py
+++ hand1.py
@@ -160,6 +160,7 @@ def scan_wifi(hashMap,_files=None,_data=None):
 
 				j["customcards"]["cardsdata"].append(c)
 			hashMap.put("toast", "Размер списка " + str(len(wifi)))
+			hashMap.put("toast", "run_sync_wifi")
 			hashMap.remove("WIFIResults")
 			hashMap.remove("WIFIConnectScan")
 			hashMap.remove("WIFIStartScan")
